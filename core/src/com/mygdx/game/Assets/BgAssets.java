a46dec025edd6c7323b8035e2a890f3ad858bbca
Nixon Deb Antu <nixondebantu@gmail.com>
2023-08-06 00:33:09 +0600

    adding assets & core file

---
+++ core/src/com/mygdx/game/Assets/BgAssets.java
@@ -0,0 +1,21 @@
+package com.mygdx.game.Assets;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+
+public class BgAssets {
+    public static Texture bgLvl1_1 = new Texture("bg/bgLvl1.1.jpg");
+    public static Texture bgLvl1_2 = new Texture("bg/bgLvl1.2.jpg");
+    public static Texture bgLvl2 = new Texture("bg/bgLvl2.jpg");
+    public static Texture bgLvl3 = new Texture("bg/bgLvl3.jpg");
+    public static Texture bgMenu = new Texture("bg/bgMenu.jpg");
+    public static Texture activeInsBtn = new Texture("menuBtn/activeInsBtn.png");
+    public static Texture laserB = new Texture("bg/LaserB.png");
+    public static Texture laserG = new Texture("bg/LaserG.png");
+    public static Sound clickSound = Gdx.audio.newSound(Gdx.files.internal("sounds/click.mp3"));
+    public static Texture[] asteroids = new Texture[4];
+
+    public static Texture coin = new Texture("coin.png");
+
+}
---
+++ core/src/com/mygdx/game/MyGame.java
@@ -0,0 +1,33 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.game.Screens.MainGameScreen;
+import com.mygdx.game.Screens.MainMenuScreen;
+
+public class MyGame extends Game {
+	public SpriteBatch batch;
+	public static final int WIDTH = 1280;
+	public static final int HEIGHT = 720;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		this.setScreen(new MainMenuScreen(this));
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
